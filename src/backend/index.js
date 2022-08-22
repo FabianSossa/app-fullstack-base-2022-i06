5ab2dfde216bac70d5ae1877dd709382c76d6039
Fabian Sossa <fabiancaihuarasossa@gmail.com>
2022-08-22 00:33:24 -0300

    Backend funcionando y probado cada operacion de la API

--- src/backend/index.js
+++ src/backend/index.js
@@ -13,25 +13,72 @@ app.use(express.static('/home/node/app/static/'));
 
 //=======[ Main module code ]==================================================
 
-app.get('/devices/', function(req, res, next) {
-    devices = [
-        { 
-            'id': 1, 
-            'name': 'Lampara 1', 
-            'description': 'Luz living', 
-            'state': 0, 
-            'type': 1, 
-        },
-        { 
-            'id': 2, 
-            'name': 'Ventilador 1', 
-            'description': 'Ventilador Habitacion', 
-            'state': 1, 
-            'type': 2, 
-        },
-    ]
-    res.send(JSON.stringify(devices)).status(200);
+// get list devices of db table Devices
+//==============[Peticion GET => Obtener información de los dispositivo] ============================
+app.get('/devices/', function(req, res) {
+    
+    console.log("Get list devices!");
+    utils.query('SELECT id, name, description, state, type from Devices',(err,rows) => {
+        if(err){
+            throw err;
+            res.send(err).status(400);
+            return
+        }
+        res.send(JSON.stringify(rows)).status(200);
+    });
 });
+//==============[Peticion GET => Obtener información de un dispositivo] ============================
+app.get('/getDevice/',function(req,res){
+    console.log("Get device info");
+    utils.query('SELECT id, name, description, state, type from Devices where id = ?', req.query.idDevice, (err,rows) => {
+        if(err){
+            throw err;
+            res.send(err).status(400);
+            return
+        }
+        res.send(JSON.stringify(rows)).status(200);
+    });
+});
+//==============[Peticion POST => Para actualizar los valores de un dispositivo] ============================
+app.post('/updateDevice/', function(req,res){
+    console.log("Update device");
+    utils.query('update Devices set name = ?, description = ?, state = ?, type = ? where id = ?',
+    [req.query.name, req.query.description, req.query.state, req.query.type, req.query.idDevice],(err,response) =>{
+        if(err){
+            throw err;
+            res.send(err).status(400);
+            return
+        }
+        res.send(JSON.stringify(response)).status(200);
+    });
+});
+//==============[Peticion POST => Agregar un nuevo ispositivo en la base de datos] ============================
+app.post('/newDevice/', function(req,res){
+    console.log("New register of Device.");
+    utils.query('insert into Devices(name, description, state, type) values(?,?,?,?)',
+    [req.query.name, req.query.description, req.query.state, req.query.type],(err,response) =>{
+        if(err){
+            throw err;
+            res.send(err).status(400);
+            return
+        }
+        res.send(JSON.stringify(response)).status(200);
+    });
+});
+//==============[Peticion POST => Para actualizar el valor de estado de un dispositivo] ============================
+app.post('/updateStateDev/',function(req,res){
+    console.log("Update device");
+    utils.query('update Devices set state = ? where id = ?',
+    [req.query.state, req.query.idDevice],(err,response) =>{
+        if(err){
+            throw err;
+            res.send(err).status(400);
+            return
+        }
+        res.send(JSON.stringify(response)).status(200);
+    });
+});
+
 
 app.listen(PORT, function(req, res) {
     console.log("NodeJS API running correctly");
